697b0cc5ebf75a5613b74cb53efbe7faadd21f33
Aghent47 <neiderkk@gmail.com>
2024-03-14 23:56:35 -0500

    Front-End: Conectar un usuario

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,17 +1,27 @@
-export const socketController = (client) => {
+import Usuarios  from '../classes/usuarios.js'
+
+const usuarios = new Usuarios(); // solo se ejecuta cuando el servidor se levanta
+
+export const socketController = (client = new Socket(), io) => {
 
     console.log('Client connected', client.id);
     
-    client.on('disconnect', () => {
-        console.log('Client disconnected', client.id);
-    });
+    // client.on('disconnect', () => {
+    //     console.log('Client disconnected', client.id);
+    // });
 
-    client.on('enviar-mensaje', ( payload, callback ) => {
+    client.on('entrarChat', (usuario, callback) => {
 
-        const id = 123456;
-        callback( id );
+        if( !usuario.nombre ){
+            return callback({
+                error: true,
+                mensaje: 'El nombre es necesario'
+            });
+        }
 
-        client.broadcast.emit('enviar-mensaje', payload);
-    
-    });
+       let personas = usuarios.agregarPersona(client.id, usuario.nombre);
+
+       callback(personas);
+    })
+
 }
